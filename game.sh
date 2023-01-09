acca823984c13b784615b8d4fc81f7648961b09c
EdDanielyan <102631130+EdDanielyan@users.noreply.github.com>
2023-01-09 13:38:24 +0400

    Update game.sh

--- game.sh
+++ game.sh
@@ -8,38 +8,36 @@ while [ "$ch" -ne 3 ]; do
          HELP : Hit 2 and enter.
          EXIT : Hit 3 and enter.
          "
-    read -p\ "Enter your choice : " ch
+    read -rp "Enter your choice : " ch
     if [ "$ch" -eq 1 ];then
     x=0 ;c=0 ;p=0
-    read -p\ "Enter any number between 0 and 9 : " n
+    read -rp "Enter any number between 0 and 9 : " n
     while [ $c -eq 0 ];
     do
-        x=11; r="$(shuf -i 0-9 -n 10)"
-        echo "$*{r[@]} "; 
-        
-        for i in ${1..10}; do
-            a"$p=$i"
+        x=11; r=("$(shuf -i 0-9 -n 10)")
+        echo "${r[@]}"; for i in {1..10}; do
+            a[i]=$i
         done
-        echo "$*{a[@]} "
-        read -t 5 -p\ "Enter the index of your number : " x
+        echo "${a[@]}"
+        read -t 5 -rp "Enter the index of your number : " x
     if [[ $? -gt 128 ]]; then
     c=1
     break
     fi
-if [ "${r[$((x))-1]}" -eq "$n" ];then
+if [[ "${r[$(("$x"))-1]}" -eq "$n" ]]; then
             echo "Great"
-    ((p=p+1))
+    p=$( (p+1) )
         else
             c=1
             break
         fi
     done
-    elif [ "$ch" -eq 2 ];then
+    elif [[ "$ch" -eq 2 ]];then
         echo "HELP: INSTRUCTIONS TO PLAY THE GAME. "
 else
         break
 fi
-if [ "$c" -eq 1 ];then
+if [[ "$c" -eq 1 ]];then
             echo -e "\nGAME OVER\n"
             echo "You scored $p points"
 fi
